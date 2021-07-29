009cde97cae0da49f516e577f019a95bc0682da6
João Bispo <joaobispo2077@gmail.com>
2021-07-29 10:59:17 -0300

    fix: add future Classic Editor path

--- plugins/wysiwyg/admin/src/components/CKEditor/index.js
+++ plugins/wysiwyg/admin/src/components/CKEditor/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
-import ClassicEditor from '../../../../../../ckeditor5-custom/build/ckeditor';
+import ClassicEditor from '../../../../../../../../ckeditor5-custom/build/ckeditor';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
